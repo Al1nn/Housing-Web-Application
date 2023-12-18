32ec5120373ed2e464419465f795dd9974b6fa19
Al1nn <88394318+Al1nn@users.noreply.github.com>
2023-12-18 22:19:17 +0200

    End of tutorial 7 + added images

--- Frontend/src/app/property/property-card/property-card.component.ts
+++ Frontend/src/app/property/property-card/property-card.component.ts
@@ -7,12 +7,13 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 
 export class PropertyCardElement implements OnInit {
-  @Input() property : any //Input, poate sa imprumute proprietatile obiectului Property
+  @Input() property_index : any //Input, poate sa imprumute proprietatile obiectului Property
     Property : any = {
         "Id" : 1,
         "Type" : "House",
         "Name" : "Birla House",
-        "Price" : 12000
+        "Price" : 12000,
+        "Image" : "assets/images/house_default.png"
     }
 
     constructor() { }
--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -12,37 +12,43 @@ export class PropertyListComponent implements OnInit {
     "Id" : 1,
     "Type" : "House",
     "Name" : "Birla House",
-    "Price" : 15000
+    "Price" : 15000,
+    "Image" : "assets/images/house_default.png"
     },
     {
       "Id" : 2,
       "Type" : "House",
       "Name" : "Erose Villa",
-      "Price" : 20000
+      "Price" : 20000,
+      "Image" : "assets/images/house_default2.png"
     },
     {
       "Id" : 3,
       "Type" : "House",
       "Name" : "Aston Villa",
-      "Price" : 5000
+      "Price" : 5000,
+      "Image" : "assets/images/house_default.png"
     },
     {
       "Id" : 4,
       "Type" : "House",
       "Name" : "Vice City Estate",
-      "Price" : 10000
+      "Price" : 10000,
+      "Image" : "assets/images/house_default2.png"
     },
     {
       "Id" : 5,
       "Type" : "House",
       "Name" : "Addams Property",
-      "Price" : 8000
+      "Price" : 8000,
+      "Image" : "assets/images/house_default.png"
     },
     {
       "Id" : 6,
       "Type" : "House",
       "Name" : "Miami Estate",
-      "Price" : 15000
+      "Price" : 15000,
+      "Image" : "assets/images/house_default2.png"
     },
 ]
   constructor() { }
