52e015e54faf9b2695fcd38840463f2278603bcb
Al1nn <88394318+Al1nn@users.noreply.github.com>
2023-12-19 23:32:50 +0200

    End of Tutorial 12 with some own modifications

--- Frontend/src/app/property/add-property/add-property.component.ts
+++ Frontend/src/app/property/add-property/add-property.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-add-property',
   templateUrl: './add-property.component.html',
   styleUrls: ['./add-property.component.css']
 })
 export class AddPropertyComponent implements OnInit {
-
+  binding : string = "wowowowow";
   constructor() { }
 
   ngOnInit() {
