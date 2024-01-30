db99d5b94d0af093b9330c63efdfa13797c82ec9
Al1nn <artistulambitios@gmail.com>
2024-01-30 14:30:03 +0200

    Ignored bin and obj

--- WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+596cf744b2c2cfbfd3ddf3351ce168a073df018a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd18f764eea3d241cb2923961b06f98f0258d479")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
