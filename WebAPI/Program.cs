f4ba3152d15cfdc19a205b70752d53cef95e9690
chirita marin <artistulambitios@gmail.com>
2024-02-11 19:58:34 +0200

    End of tutorial 45

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
@@ -12,8 +13,18 @@ using WebAPI.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Host.ConfigureHostConfiguration(webBuilder => { webBuilder.AddEnvironmentVariables(prefix: "MYFIRSTAPP_"); });
+
 // Add services to the container.
-builder.Services.AddDbContext<DataContext>( options => options.UseSqlServer( builder.Configuration.GetConnectionString("Default") ) );
+var connectionBuilder = new SqlConnectionStringBuilder(
+        builder.Configuration.GetConnectionString("Default"));
+
+connectionBuilder.Password = builder.Configuration.GetSection("DBPassword").Value;
+
+var connectionString = connectionBuilder.ConnectionString;
+
+
+builder.Services.AddDbContext<DataContext>( options => options.UseSqlServer(connectionString) );
 builder.Services.AddControllers().AddNewtonsoftJson();
 builder.Services.AddCors();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
