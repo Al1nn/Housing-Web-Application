19d7e213487e17c65e465aca8ea86e23973bf084
chirita marin <artistulambitios@gmail.com>
2024-06-10 16:01:32 +0300

    Pagination Service Implemented

--- Frontend/src/app/services/housing.service.ts
+++ Frontend/src/app/services/housing.service.ts
@@ -57,6 +57,11 @@ export class HousingService {
         return this.http.get<Property[]>(this.baseUrl + '/property/list/' + SellRent?.toString());
     }
 
+
+    getPaginatedProperty(SellRent: number, PageNumber: number, PageSize: number): Observable<Property[]> {
+        return this.http.get<Property[]>(this.baseUrl + '/property/list/' + SellRent?.toString() + '/' + PageNumber?.toString() + '/' + PageSize?.toString());
+    }
+
     getUserProperties(): Observable<Property[]> {
         const httpOptions = {
             headers: new HttpHeaders({
