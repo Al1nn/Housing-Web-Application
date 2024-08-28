59f0369bfd6f3a14c15c6eb54217da94ba017143
chirita marin <artistulambitios@gmail.com>
2024-08-28 16:42:13 +0300

    Changed Design of Filters

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
-export const environment ={
-    production : true,
-    baseUrl : 'https://myfirstcsharpapi.azurewebsites.net/api'
+export const environment = {
+    production: true,
+    baseUrl: 'https://myfirstcsharpapi.azurewebsites.net/api',
+    thumbnailFolder: 'https://myfirstcsharpapi.azurewebsites.net/UPLOADS/thumbnails/',
+    originalPictureFolder: 'https://myfirstcsharpapi.azurewebsites.net/UPLOADS/originalSizes/'
 };
