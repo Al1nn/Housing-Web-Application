5c1ea9f0f8414f277b08c75acd8892a6c197a13c
Al1nn <artistulambitios@gmail.com>
2024-11-13 16:18:49 +0200

    Commit from macOS

---
+++ WebAPI/Controllers/Migrations/20240322153218_UserImage.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserImage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Images",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Url = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    LastUpdatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastUpdatedBy = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Images", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserImage",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    ImageId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserImage", x => new { x.UserId, x.ImageId });
+                    table.ForeignKey(
+                        name: "FK_UserImage_Images_ImageId",
+                        column: x => x.ImageId,
+                        principalTable: "Images",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserImage_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserImage_ImageId",
+                table: "UserImage",
+                column: "ImageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserImage_UserId",
+                table: "UserImage",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserImage");
+
+            migrationBuilder.DropTable(
+                name: "Images");
+        }
+    }
+}
---
+++ WebAPI/Controllers/Migrations/20240322154324_DbSetUserImages.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class DbSetUserImages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserImage_Images_ImageId",
+                table: "UserImage");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserImage_Users_UserId",
+                table: "UserImage");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserImage",
+                table: "UserImage");
+
+            migrationBuilder.RenameTable(
+                name: "UserImage",
+                newName: "UserImages");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserImage_ImageId",
+                table: "UserImages",
+                newName: "IX_UserImages_ImageId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserImages",
+                table: "UserImages",
+                columns: new[] { "UserId", "ImageId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserImages_Images_ImageId",
+                table: "UserImages",
+                column: "ImageId",
+                principalTable: "Images",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserImages_Users_UserId",
+                table: "UserImages",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserImages_Images_ImageId",
+                table: "UserImages");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserImages_Users_UserId",
+                table: "UserImages");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserImages",
+                table: "UserImages");
+
+            migrationBuilder.RenameTable(
+                name: "UserImages",
+                newName: "UserImage");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserImages_ImageId",
+                table: "UserImage",
+                newName: "IX_UserImage_ImageId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserImage",
+                table: "UserImage",
+                columns: new[] { "UserId", "ImageId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserImage_Images_ImageId",
+                table: "UserImage",
+                column: "ImageId",
+                principalTable: "Images",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserImage_Users_UserId",
+                table: "UserImage",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
