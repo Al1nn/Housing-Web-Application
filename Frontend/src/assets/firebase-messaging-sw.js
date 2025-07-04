57db5250e5435ef2823659d3bf40929322af6130
Al1nn <artistulambitios@gmail.com>
2025-07-04 15:45:45 +0300

    Stable Build

--- Frontend/src/assets/firebase-messaging-sw.js
+++ Frontend/src/assets/firebase-messaging-sw.js
@@ -4,7 +4,15 @@ importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
 
 
 // Your Firebase configuration
-const firebaseConfig = { "projectId": "bla", "appId": "bla", "databaseURL": "bla", "storageBucket": "bla", "apiKey": "bla", "authDomain": "bla", "messagingSenderId": "bla" };
+const firebaseConfig = {
+  projectId: 'myfirstapp-a15e1',
+  appId: '1:859208953616:web:f9a1026cb308c49963bf95',
+  databaseURL: 'https://myfirstapp-a15e1-default-rtdb.europe-west1.firebasedatabase.app',
+  storageBucket: 'myfirstapp-a15e1.appspot.com',
+  apiKey: 'AIzaSyAN3_n0tOjV_tJ37vc33Xpa9Kbr5ERSRGE',
+  authDomain: 'myfirstapp-a15e1.firebaseapp.com',
+  messagingSenderId: '859208953616',
+};
 
 firebase.initializeApp(firebaseConfig);
 
